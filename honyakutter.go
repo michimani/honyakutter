c3efa797fcd68c4f713e76fee5144d72da42b443
michimani <michimani210@gmail.com>
2021-12-03 00:28:23 +0900

    remove example code

--- honyakutter.go
+++ honyakutter.go
@@ -4,8 +4,6 @@ import (
 	"honyakutter/resources"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 )
 
@@ -20,13 +18,8 @@ func NewHonyakutterStack(scope constructs.Construct, id string, props *Honyakutt
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	// Tweet Lambda Function
 	resources.TweetLambdaFunction(stack)
-
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("HonyakutterQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
 
 	return stack
 }
@@ -46,26 +39,5 @@ func main() {
 // env determines the AWS environment (account+region) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
 	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
 }
